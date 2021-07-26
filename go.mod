7f4360a7dc954b772cefa205e8d5d20a583bd09e
Pedro Luis Reyes Luna <pereyes@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-07-26 11:54:18 -0500

    test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/PReyesMELI/testGo
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+)
+
+func init() {
+	fmt.Println("iniciando")
+}
+
+func main() {
+	fmt.Println("hello world")
+}
